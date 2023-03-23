905b2aa9fad8af8819157594cf9b612b0d72fdeb
ChewieTheShiba <128498743+ChewieTheShiba@users.noreply.github.com>
2023-03-23 17:56:43 -0400

    Update Hitbox.java

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -1,6 +1,7 @@
 public class Hitbox {
 
 	private int h, k, a, b, startx, starty;
+	private double[] latestIntersection = {-1,-1};
 	
 	public Hitbox(int a, int b, int h, int k)
 	{
@@ -13,9 +14,9 @@ public class Hitbox {
 	public double[] intersects(Hitbox h1)
 	{
 		double d[] = {-1, -1};
-		for(double q = h-(a/2.0); q <= h+(a/2.0); q += 0.1)
+		for(double q = h-(a/2.0); q <= h+(a/2.0); q += 10)
 		{
-	        for(double w = h1.getH()-(h1.getA()/2.0); w <= h1.getH()+(h1.getA()/2.0); w += 0.1)
+	        for(double w = h1.getH()-(h1.getA()/2.0); w <= h1.getH()+(h1.getA()/2.0); w += 10)
 	          {
 	            int ha = h1.getA();
 	            int hb = h1.getB();
@@ -39,22 +40,63 @@ public class Hitbox {
 	            double negy2 = hk - Math.sqrt((ab-bb)/(ha*ha));
 	            System.out.println("negy2\t" + negy2);
 	            
-	            if(Math.abs(q-w) < 0.1 && Math.abs(y2-y1) < 0.1 || Math.abs(q-w) < 0.1 && Math.abs(negy2-y1) < 0.1)
+	            if(Math.abs(q-w) < 10 && Math.abs(y2-y1) < 10 || Math.abs(q-w) < 10 && Math.abs(negy2-y1) < 10)
 	            {
 	              d[0] = q;
 	              d[1] = y1;
+	              latestIntersection = d;
 	              return d;
 	            }
-	            if(Math.abs(q-w) < 0.1 && Math.abs(y2-negy1) < 0.1 || Math.abs(q-w) < 0.1 && Math.abs(y2-y1) < 0.1)
+	            if(Math.abs(q-w) < 10 && Math.abs(y2-negy1) < 10 || Math.abs(q-w) < 10 && Math.abs(y2-y1) < 10)
 	            {
 	              d[0] = q;
 	              d[1] = negy1;
+	              latestIntersection = d;
 	              return d;
 	            }
 	          }
 		}
     return d;
 	}
+	
+	
+	public double[] getOppositeInteresection()
+	{
+		double d[] = {-1,-1};
+		if(latestIntersection != d)
+		{
+			if(latestIntersection[0]-h < 0)
+			{
+				d[0] = latestIntersection[0]-h + (((latestIntersection[0]-h)*2)*-1);
+				System.out.println(d[0]);
+				d[0] += h;
+			}
+			else if(latestIntersection[0]-h > 0)
+			{
+				d[0] = latestIntersection[0]-h - ((latestIntersection[0]-h)*2);
+				System.out.println(d[0]);
+				d[0] += h;
+			}
+			if(latestIntersection[1]-k < 0)
+			{
+				d[1] = latestIntersection[1]-k + (((latestIntersection[1]-k)*2)*-1);
+				System.out.println(d[1]);
+				d[1] += k;
+				return d;
+			}
+			else if(latestIntersection[1]-k > 0)
+			{
+				d[1] = latestIntersection[1]-k - ((latestIntersection[1]-k)*2);
+				System.out.println(d[1]);
+				d[1] += k;
+				return d;
+			}
+		}
+		
+		return d;
+	}
+	
+	
 
   public int getA()
   {
