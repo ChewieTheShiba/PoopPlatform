7744fd77d86521f2e9ac7f141c8892eb538179f5
ChewieTheShiba <128498743+ChewieTheShiba@users.noreply.github.com>
2023-03-23 10:59:33 -0400

    t

---
+++ src/Hitbox.java
@@ -0,0 +1,19 @@
+public class Hitbox {
+
+	private int h, k, a, b, startx, starty;
+	
+	public Hitbox(int a, int b, int x, int y)
+	{
+		this.a = a;
+		this.b = b;
+		startx = x;
+		starty = x;
+		h = startx+(a/2);
+		k = starty+(b/2);
+	}
+	
+	public boolean intersects(Hitbox h)
+	{
+		for(int )
+	}
+}
