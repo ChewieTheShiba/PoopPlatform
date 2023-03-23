434c3ef1cac7a2d66f96a2b8e96be28968e5339d
ChewieTheShiba <128498743+ChewieTheShiba@users.noreply.github.com>
2023-03-22 21:10:30 -0400

    Loading Screen
    Made a loading screen I think with filler images imma ask slata tho on how to move JLabels around cuz i need

--- src/PoopPanel.java
+++ src/PoopPanel.java
@@ -1,10 +1,15 @@
 import java.awt.*;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.Timer;
 
 public class PoopPanel extends JPanel
 {
 	//variables for the overall width and height
 	private int w, h;
+	private ImageIcon startUpAnimation, startUpScreen;
+	private JLabel startUpImage;
+	private Timer startUpWait;
 	
 	//sets up the initial panel for drawing with proper size
 	public PoopPanel(int w, int h)
@@ -13,6 +18,16 @@ public class PoopPanel extends JPanel
 		this.h = h;
 		this.setPreferredSize(new Dimension(w,h));
 		
+		/*sets up startup animation and wait and then waiting screen
+		Animation will play and then once it stops screen will appear
+		and wait for the player to press start
+		*/
+		startUpAnimation = new ImageIcon("assets/startUpAnimation.gif");
+		startUpScreen = new ImageIcon("assets/startUpScreen.png");
+		startUpImage = new JLabel(startUpAnimation);
+		startUpWait = new Timer(5000, new actionListen());
+		startUpWait.start();
+		this.add(startUpImage);
 	}
 	
 	
@@ -24,8 +39,24 @@ public class PoopPanel extends JPanel
 		super.paintComponent(g);
 		
 		//all drawings below here:
-		
-		
-		
 	}
+	
+	private class actionListen implements ActionListener
+	{
+
+		public void actionPerformed(ActionEvent e)
+		{
+			Object source = e.getSource();
+			
+			//changes startup image to the waiting screen
+			if(source.equals(startUpWait))
+			{
+				startUpWait.stop();
+				startUpImage.setIcon(startUpScreen);
+			}
+			
+		}
+	}
+	
+	
 }
