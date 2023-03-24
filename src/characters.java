11d10be860ae7d0162686d04ea81367f24a5423b
RazersTv <charlie.t.connolly@icloud.com>
2023-03-24 12:23:17 -0400

    Create characters.java

---
+++ src/characters.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class characters {
+    double hitboxX, hitboxY;
+    String name, description;
+    int HP, attack1power, attack2power, attack3power, specialattackpower;
+}
