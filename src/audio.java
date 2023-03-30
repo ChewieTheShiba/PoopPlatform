3f46e93d43bfd880f76fe0cb3ad693c8772e7943
RazersTv <charlie.t.connolly@icloud.com>
2023-03-30 13:18:10 -0400

    Update audio.java

--- src/audio.java
+++ src/audio.java
@@ -1,5 +1,8 @@
 //javafx.scene.media.Media;
 
 public class audio {
-    
+    Media loading = new Media(/*enter file path here*/);
+    Media lobby = new Media(/*Enter file path here */);
+    Media punch = new Media (/*Enter file path here */);
+    Media hit = new Media(/*Enter file path here */);
 }
